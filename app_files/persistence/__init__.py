eb7e6fbeeb63eac6294fe9b9dea86c7748b7d339
hsparksjr1980-stack <hsparksjr1980@gmail.com>
2026-05-13 13:52:58 -0500

    Expose persistence restore helper

--- app_files/persistence/__init__.py
+++ app_files/persistence/__init__.py
@@ -1,8 +1,16 @@
 # app_files/persistence/__init__.py
 
-from .storage import delete_session, export_session_state, list_sessions, load_session, save_session
+from .storage import (
+    apply_session_to_state,
+    delete_session,
+    export_session_state,
+    list_sessions,
+    load_session,
+    save_session,
+)
 
 __all__ = [
+    "apply_session_to_state",
     "delete_session",
     "export_session_state",
     "list_sessions",
